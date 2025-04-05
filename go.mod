42897ca013350725fbe578ba67e08ca8caf510b3
pawan gupta <guptapawanro2017@gmail.com>
2025-04-05 22:53:50 +0530

    Add initial project setup with basic HTTP server
    Set up a Go module with necessary dependencies and implemented a basic HTTP server with health check and graceful shutdown support. Added local and remote Dockerfiles for containerization

---
+++ go.mod
@@ -0,0 +1,10 @@
+module taskease
+
+go 1.24.1
+
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-chi/render v1.0.3
+)
+
+require github.com/ajg/form v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,77 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// Add middleware
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	// Add health check endpoint
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		render.JSON(w, r, map[string]string{"status": "ok"})
+	})
+
+	// Start the server
+	serverAddr := fmt.Sprintf(":%s", "8080")
+	server := &http.Server{
+		Addr:         serverAddr,
+		Handler:      r,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+		// Shutdown signal with grace period of 30 seconds
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if errors.Is(shutdownCtx.Err(), context.DeadlineExceeded) {
+				log.Fatal("Graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+
+	// Start the server
+	log.Printf("Starting server on %s", serverAddr)
+	err := server.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatal(err)
+	}
+
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
+	log.Println("Server stopped")
+}
